23652937fe1b70754a5a057e65f7339f9d9152e8
blacksmithfabbro <7on.mail@gmail.com>
2026-05-17 11:33:12 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
+const { app, BrowserWindow } = require('electron');
 
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1280,
+        height: 720,
+        webPreferences: {
+            webSecurity: false,
+            contextIsolation: false
+        },
+        transparent: true,
+        frame: false
+    });
+
+    win.loadURL('https://www.donationalerts.com/widget/alerts?group_id=XXXX');
+}
+
+app.whenReady().then(createWindow);
